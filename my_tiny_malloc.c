473a169bcc058358441a3b342a6356cfe3da052d
Osmolovsky K. <kirillosm09@gmail.com>
2026-01-26 16:25:28 +1000

    fast left merge due to footer

--- my_tiny_malloc.c
+++ my_tiny_malloc.c
@@ -17,6 +17,9 @@ void heap_init(void)
     block_header* first = (block_header*)heap_start;
     first->size = HEAP_SIZE;
     first->free = 1;
+    block_header* footer = (block_header*)((char*)heap_start + first->size - sizeof(block_header));
+    footer->size = HEAP_SIZE;
+    footer->free = 1;
 }
 
 
@@ -26,26 +29,39 @@ void* my_tiny_malloc(size_t size)
     block_header* current = (block_header*) heap_start;
     while ((char*) current < (char*) heap_end) 
     {
-        if(current->free && current->size >= (size + sizeof(block_header)) )
+        if(current->free && current->size >= (size + 2 * sizeof(block_header)) )
         {
             // we found a big block!
             size_t total_block_size = current->size;
-            size_t needed_size = size + sizeof(block_header);
+            size_t needed_size = size + 2 * sizeof(block_header); // we need 2 block_headers because of header and footer on the block
 
-            if(total_block_size - needed_size >= sizeof(block_header) + MIN_DATA_SIZE)
+            if(total_block_size - needed_size >= 2 * sizeof(block_header) + MIN_DATA_SIZE)
             {
                 // split the block
                 current->size = needed_size;
                 current->free = 0;
 
+                // write footer
+                block_header* footer = (block_header*)((char*)current + current->size - sizeof(block_header));
+                footer->size = current->size;
+                footer->free = 0;
+
+                // next block from remainings
                 block_header* next_block = (block_header*)((char*)current + needed_size);
                 next_block->size = total_block_size - needed_size;
                 next_block->free = 1;
+
+                // footer at the end of the next block
+                block_header* next_footer = (block_header*)((char*)next_block + next_block->size - sizeof(block_header));
+                next_footer->size = next_block->size;
+                next_footer->free = 1;
             }
             else
             {
                 // data fits
                 current->free = 0;
+                block_header* footer = (block_header*)((char*)current + current->size - sizeof(block_header));
+                footer->free = 0;
             }
             return (void*)((char*)current + sizeof(block_header));
         }
@@ -67,29 +83,37 @@ void my_tiny_free(void* ptr)
 
     header->free = 1; // now block marked as free
 
+    block_header* footer = (block_header*)((char*)header + header->size - sizeof(block_header));
+    footer->free = 1; // mark in footer too
+
     // right merge
     char* right_address = (char*)header + header->size;
     if(right_address < (char*) heap_end)
     {
-        if(((block_header*) right_address)->free)
+        block_header* right_header = (block_header*) right_address;
+        if(right_header->free)
         {
-            header->size = header->size + ((block_header*) right_address)->size;
+            header->size += right_header->size;
+            block_header* final_footer = (block_header*)((char*)header + header->size - sizeof(block_header));
+            final_footer->size = header->size;
+            final_footer->free = 1;
         }
     }
 
     // left merge
-    block_header* current = (block_header*) heap_start;
-    while ((char*) current < (char*) header)
+    if ((char*)header > (char*)heap_start) 
     {
-        if( (char*) ((char*)current + current->size) == (char*) header)
-        {
-            if(current->free)
-            {
-                current->size = current->size + header->size;
-            }
-            
-            break;
+        // look up at the previous block footer
+        block_header* prev_footer = (block_header*)((char*)header - sizeof(block_header));
+        if (prev_footer->free) {
+            block_header* prev_header = (block_header*)((char*)header - prev_footer->size);
+            prev_header->size += header->size;
+
+            block_header* final_footer = (block_header*)((char*)prev_header + prev_header->size - sizeof(block_header));
+            final_footer->size = prev_header->size;
+            final_footer->free = 1;
+
+            header = prev_header; 
         }
-        current = (block_header*)((char*)current + current->size);
-    } 
+    }
 }
